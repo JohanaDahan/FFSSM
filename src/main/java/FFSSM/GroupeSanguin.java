bb97b4a816f9acb9136a22069f7e8c029a649203
johan <johan@LAPTOP-L98NJ3P5>
2020-11-29 11:34:25 +0100

    Merge origin/master
    Conflicts:
    	pom.xml

--- src/main/java/FFSSM/GroupeSanguin.java
+++ src/main/java/FFSSM/GroupeSanguin.java
@@ -3,20 +3,5 @@ package FFSSM;
 public enum GroupeSanguin {
 	APLUS, AMOINS, BPLUS, BMOINS;	
         
-    public static GroupeSanguin getAPLUS() {
-        return APLUS;
-    }
-
-    public static GroupeSanguin getAMOINS() {
-        return AMOINS;
-    }
-
-    public static GroupeSanguin getBPLUS() {
-        return BPLUS;
-    }
-
-    public static GroupeSanguin getBMOINS() {
-        return BMOINS;
-    }
-    
+      
 }
